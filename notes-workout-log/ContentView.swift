56548008bf185833fc74a3c228cd886666ffaf06
Marcelo Querque <querquemarcelo@gmail.com>
2025-12-18 08:04:43 -0500

    Initial Commit

---
+++ notes-workout-log/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  notes-workout-log
+//
+//  Created by Marcelo Albuquerque on 18/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ notes-workout-log/notes_workout_logApp.swift
@@ -0,0 +1,17 @@
+//
+//  notes_workout_logApp.swift
+//  notes-workout-log
+//
+//  Created by Marcelo Albuquerque on 18/12/25.
+//
+
+import SwiftUI
+
+@main
+struct notes_workout_logApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
